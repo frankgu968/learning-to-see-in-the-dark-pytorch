6a79429eb5e65c53eed5ab7123f92f78f9642a4c
Frank Gu <frank.gu968@outlook.com>
2020-11-19 17:10:33 -0500

    fix: better dataset loading procedure

--- dataset.py
+++ dataset.py
@@ -27,57 +27,53 @@ class LTSIDDataset(Dataset):
   def __init__(self, train_dir, truth_dir, transforms=None):
     self.train_dir = train_dir
     self.truth_dir = truth_dir
-    self.train_fns = glob.glob(train_dir + '0*.ARW')  # All the training filenames
-    # Create container for images
-    self.truth_images = [None] * len(self.train_fns)
+    self.train_fns = glob.glob(train_dir + '0*_00*.ARW')  # All the training filenames
+    self.truth_fns = glob.glob(truth_dir + '0*.ARW')  # All the ground truth filenames
+    # Pre-allocate lists for images
+    self.truth_images = [None] * len(self.truth_fns)
     self.train_images = [None] * len(self.train_fns)
+    self.train_truth_map = [None] * len(self.train_fns) # Array mapping a training image index to the corresponding truth index
     # Set the keys for different exposure levels
     self.load_images()
     self.transforms = transforms
 
   def load_images(self):
-    for idx, train_fn in enumerate(self.train_fns):
-      train_id = int(os.path.basename(train_fn)[0:5])
-      truth_file = glob.glob(self.truth_dir + '%05d_00*.ARW' % train_id)[0]   # 1 ground truth file
-      truth_fn = os.path.basename(truth_file)
-      truth_exposure = float(truth_fn[9:-5])
-      # Load ground-truth image
-      truth_raw = rawpy.imread(truth_file)
+    train_cnt = 0 # Index counter for the training images
+    for idx, truth_fn in enumerate(self.truth_fns):
+      train_id = int(os.path.basename(truth_fn)[0:5])
+      truth_fn = os.path.basename(truth_fn)
+      train_files = glob.glob(self.train_dir + '%05d_00*.ARW' % train_id)     # Multiple training files
+      truth_exposure = float(truth_fn[9:-5])    # Get the exposure time from the ground truth filename
+
+      # Load the ground truth image
+      truth_raw = rawpy.imread(self.truth_dir + truth_fn)
       im = truth_raw.postprocess(use_camera_wb=True, half_size=False, no_auto_bright=True, output_bps=16)
       self.truth_images[idx] = np.float32(im / 65535.0)
 
-      train_fn = os.path.basename(train_fn)
-      train_exposure = float(train_fn[9:-5])
-      ratio = min(truth_exposure / train_exposure, 300)  # Calculate exposure ratio for simple scaling
-      # Load image
-      raw = rawpy.imread(self.train_dir + train_fn)
-      self.train_images[idx] = pack_raw(raw) * ratio
+      # Load the associated training images
+      for train_fn in train_files:
+        train_fn = os.path.basename(train_fn)
+        train_exposure = float(train_fn[9:-5])
+        ratio = min(truth_exposure / train_exposure, 300)     # Calculate exposure ratio for simple scaling
+        raw = rawpy.imread(self.train_dir + train_fn)         # Load image
+        self.train_images[train_cnt] = pack_raw(raw) * ratio  # Scale the pixel values by the exposure time ratio
+        self.train_truth_map[train_cnt] = idx                 # Set the index of the corresponding ground truth image
+        train_cnt = train_cnt + 1
 
   def __len__(self):
     return len(self.train_images)
 
   def __getitem__(self, idx):
+    truth_idx = self.train_truth_map[idx]
     sample = {
       'train': self.train_images[idx],
-      'truth': self.truth_images[idx]
+      'truth': self.truth_images[truth_idx]
     }
 
     if self.transforms:
-      sample = self.transforms(sample)
+      sample = {
+        'train': self.transforms(sample['train']),
+        'truth': self.transforms(sample['truth'])
+      }
 
     return sample
-
-class ToTensor(object):
-  """Convert ndarrays in sample to Tensors."""
-
-  def __call__(self, sample):
-    train, truth = sample['train'], sample['truth']
-
-    # swap color axis because
-    # numpy image: H x W x C
-    # torch image: C X H X W
-    # train = train.transpose((2, 0, 1))
-    # truth = truth.transpose((2, 0, 1))
-    return {'train': torch.from_numpy(train),
-            'truth': torch.from_numpy(truth),
-            }
--- main.py
+++ main.py
@@ -1,18 +1,25 @@
 import torch
 from model.model import UNet
-from dataset import LTSIDDataset, ToTensor
+from dataset import LTSIDDataset
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
+import numpy as np
 
 # test_input = torch.rand(1, 4, 512, 512)
 # model = UNet()
 # output = model.forward(test_input)
 
 # print(output.shape)
+np.random.seed(0) # Deterministic random
 
 train_dir = './data/short/'
 truth_dir = './data/long/'
-dataset = LTSIDDataset(train_dir, truth_dir, transforms=transforms.Compose([ToTensor()]))
+patch_size = 512
+dataset = LTSIDDataset(train_dir, truth_dir, 
+                        transforms=transforms.Compose([
+                                                        transforms.ToTensor(),
+                                                        transforms.RandomCrop(patch_size),
+                                                      ]))
 dataloader = DataLoader(dataset, batch_size=15, shuffle=False)
 
 dataloader_iterator = iter(dataloader)
@@ -20,7 +27,5 @@ data = next(dataloader_iterator)
 print(data['train'].shape)
 print(data['truth'].shape)
 
-print(torch.all(torch.eq(data['truth'][0], data['truth'][11])))
-print(torch.all(torch.eq(data['truth'][10], data['truth'][11])))
-print(torch.all(torch.eq(data['truth'][11], data['truth'][12])))
+print(torch.all(torch.eq(data['truth'][1], data['truth'][0])))
 
