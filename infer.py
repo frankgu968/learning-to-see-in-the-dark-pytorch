4c9320442e1df60d69cee1a91060e7186f4205c3
SEANDOUGHTYEDL <sean.doughty@mail.utoronto.ca>
2020-11-24 22:28:56 -0500

    chore: comments and user feedback

--- infer.py
+++ infer.py
@@ -14,22 +14,30 @@ import time
 import sys
 
 checkpoint_path = './checkpoint/checkpoint.t7'
-#image_path = './data/inference/input/20001_00_0.03s.ARW'
-#ratio = 250
 
 if __name__ == '__main__':
-    image_path = sys.argv[1]
-    ratio = int(sys.argv[2])
+    try:
+        image_path = sys.argv[1]
+        output_path = sys.argv[2]
+        ratio = int(sys.argv[3])
+    except:
+        print("Error in inference input, use command:\n $ python infer.py RAW_IMAGE_PATH OUTPUT_IMAGE EXPOSURE_RATIO")
+        sys.exit()
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
+    # load model
     model = UNet().to(device)
     model.load_state_dict(torch.load(checkpoint_path)["state_dict"])
+
+    #set model to evaluate mode
     model.eval()
 
+    # image import
     raw = rawpy.imread(image_path) 
     im = pack_raw(raw) * ratio
 
+    # scaling image down to a max dimension of 1024, maintaining aspect ratio
     if max(im.shape) > 1024:
         scale_factor = 1024 / max(im.shape)
         H = int(im.shape[0] * scale_factor)
@@ -37,10 +45,12 @@ if __name__ == '__main__':
         im = cv2.resize(im, (W,H), cv2.INTER_AREA)
 
 
+    # cropping image to nearest 16, to allow torch to compute
     H = math.floor(im.shape[0]/16.0)*16
     W = math.floor(im.shape[1]/16.0)*16
     im = im[:H, :W, :]
 
+    # transpose and add dummy sample dimension
     tensor = torch.from_numpy(im).transpose(0, 2).unsqueeze(0)
     tensor = tensor.to(device)
 
@@ -49,4 +59,7 @@ if __name__ == '__main__':
         output = output.to('cpu').numpy() * 255
         output = output.squeeze()
         output = np.transpose(output, (2, 1, 0)).astype('uint8')
-        Image.fromarray(output).convert("RGB").show()
+        output = Image.fromarray(output).convert("RGB")
+        output.show()
+        output.save(output_path)
+        
